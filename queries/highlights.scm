fb04b029e2b4ae38baaec860525d8d7c026d2a08
Chiru B <33750251+itsezc@users.noreply.github.com>
2026-02-19 03:26:45 +0530

    add: `fn::*` highlights

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,13 +1,39 @@
 [
   (keyword_computed)
   (keyword_reference)
-  (keyword_on)
-  (keyword_delete)
-  (keyword_ignore)
-  (keyword_unset)
-  (keyword_cascade)
-  (keyword_reject)
 ] @keyword
 
-((variable_name) @variable.reserved
-  (#match? @variable.reserved "^\\$(this|auth|action|file|target|value|parent|access|event|before|after|request|reference|token|session|input)$"))
+[
+  (keyword_none)
+] @constant.builtin
+
+(reference_on_delete_clause
+  (keyword_on) @constant.builtin
+  (keyword_delete) @constant.builtin)
+
+(reference_on_delete_clause
+  [
+    (keyword_ignore)
+    (keyword_unset)
+    (keyword_cascade)
+    (keyword_reject)
+  ] @keyword)
+
+(reference_on_delete_clause
+  (keyword_then) @keyword.control)
+
+(function_call
+  (custom_function_name) @function)
+
+(function_call
+  (function_name) @function)
+
+(function_call
+  (function_name) @function.builtin
+  (#match? @function.builtin "^(array|crypto|duration|encoding|geo|http|math|meta|not|object|parse|rand|record|search|session|sleep|string|time|type|vector)::[a-zA-Z_][a-zA-Z0-9_]*$"))
+
+(function_call
+  (function_name) @function.special
+  (#match? @function.special "^(array|crypto|duration|encoding|geo|http|math|meta|not|object|parse|rand|record|search|session|sleep|string|time|type|vector)::[a-zA-Z_][a-zA-Z0-9_]*::[a-zA-Z_][a-zA-Z0-9_]*$"))
+
+(variable_name) @variable.parameter

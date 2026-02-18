8e0ed02dd15b420b950eef59cdbbc903792756d8
Chiru B <33750251+itsezc@users.noreply.github.com>
2026-02-19 02:58:11 +0530

    add: `COMPUTED` and `REFERENCE` keywords

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,2 +1,13 @@
+[
+  (keyword_computed)
+  (keyword_reference)
+  (keyword_on)
+  (keyword_delete)
+  (keyword_ignore)
+  (keyword_unset)
+  (keyword_cascade)
+  (keyword_reject)
+] @keyword
+
 ((variable_name) @variable.reserved
   (#match? @variable.reserved "^\\$(this|auth|action|file|target|value|parent|access|event|before|after|request|reference|token|session|input)$"))
